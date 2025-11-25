9cbfc7270de2ceba029f297b883947aad12b66eb
PaulP1406 <ducduy0308@gmail.com>
2025-11-24 18:08:17 -0800

    pcap file

---
+++ README.md
@@ -0,0 +1,1 @@
+To view the detailed pcap files, use the filter udp.port == 5000
--- sender_test.py
+++ sender_test.py
@@ -4,9 +4,7 @@ from sender import Sender
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setblocking(False)
 
-# Replace with the receiver's actual IP address
-RECEIVER_IP = "192.168.1.100"  # Change this to receiver's IP
-sender = Sender(sock, (RECEIVER_IP, 5000), window_size=4)
+sender = Sender(sock, ("localhost", 5000), window_size=4)
 
 sender.connect()  # <-- Perform handshake before sending
 
